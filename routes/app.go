0e56a161570d20f4862c2413c9363593decb3784
Ismail <ismailsmakhus@gmail.com>
2021-11-17 21:03:39 +0700

    update routes

--- routes/app.go
+++ routes/app.go
@@ -1,14 +1,24 @@
 package routes
 
 import (
-
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+
+	"gin-weight-tracker/app/controllers"
 )
 
 func MakeAppRoutes(r *gin.Engine) {
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello!")
-	})
+	HomeController := controllers.HomeController{}
+	ProfileController := controllers.ProfileController{}
+	WeightController := controllers.WeightController{}
+
+	r.GET("/", HomeController.Index)
+	r.GET("/login", HomeController.Login)
+	r.POST("/login", HomeController.DoLogin)
+
+	r.GET("/profile", ProfileController.Index)
+	r.GET("/profile/create", ProfileController.Create)
+	r.POST("/profile/store", ProfileController.Store)
+
+	r.GET("/weight/create", WeightController.Create)
+	r.POST("/weight/store", WeightController.Store)
 }
